0308e5e68b5c9f163246459f21281d3b12a629a0
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-08 19:16:35 +0800

    feat: add ParkingBoy

---
+++ src/main/java/com/afs/parkinglot/ParkingBoy.java
@@ -0,0 +1,19 @@
+package com.afs.parkinglot;
+
+
+public class ParkingBoy {
+    private final ParkingLot parkingLot;
+
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+/*
+    public Ticket helpParkCar(Car car) {
+        return parkingLot.parking(car);
+    }*/
+
+/*    public Car helpFetchCar(Ticket ticket) {
+        return parkingLot.fetch(ticket);
+    }*/
+}
