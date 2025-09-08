b31c105020ee1b4b1d0ccc8aed9969cad41b4ae6
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-08 19:42:21 +0800

    feat: add ParkingLotManager

---
+++ src/main/java/com/afs/parkinglot/ParkingLotManager.java
@@ -0,0 +1,11 @@
+package com.afs.parkinglot;
+
+import java.util.List;
+
+public class ParkingLotManager {
+    private final List<ParkingLot> parkingLots;
+
+    public ParkingLotManager(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
+}
