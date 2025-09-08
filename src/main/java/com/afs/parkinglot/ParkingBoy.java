f18db523d64c074ab185cadaf6d88fe7162b6cbc
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-08 19:17:31 +0800

    test: should_return_ticket_when_parking_boy_helps_park_car

--- src/main/java/com/afs/parkinglot/ParkingBoy.java
+++ src/main/java/com/afs/parkinglot/ParkingBoy.java
@@ -8,10 +8,9 @@ public class ParkingBoy {
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
-/*
     public Ticket helpParkCar(Car car) {
         return parkingLot.parking(car);
-    }*/
+    }
 
 /*    public Car helpFetchCar(Ticket ticket) {
         return parkingLot.fetch(ticket);
---
+++ src/test/java/com/afs/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,18 @@
+package com.afs.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingBoyTest {
+    @Test
+    public void should_return_ticket_when_parking_boy_helps_park_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car("parking number 1");
+
+        Ticket ticketResult = parkingBoy.helpParkCar(car);
+        Ticket ticket = new Ticket(car, 1, parkingLot);
+        assertEquals(car, ticket.car());
+        assertEquals(ticketResult, ticket);
+    }
+}
