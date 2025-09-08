532d33813c97e00af28cca41bb8bea103c97f9af
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-08 19:49:35 +0800

    test: ParkingLotManagerTest should_throw_exception_when_all_lots_are_full

--- src/test/java/com/afs/parkinglot/ParkingLotManagerTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotManagerTest.java
@@ -27,4 +27,18 @@ public class ParkingLotManagerTest {
         Ticket ticket = manager.helpParkCar(car);
         assertEquals(lot2, ticket.parkingLot());
     }
+
+    @Test
+    public void should_throw_exception_when_all_lots_are_full() {
+        ParkingLot lot1 = new ParkingLot(0);
+        ParkingLot lot2 = new ParkingLot(0);
+        ParkingLotManager manager = new ParkingLotManager(Arrays.asList(lot1, lot2));
+        Car car = new Car("car3");
+
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> manager.helpParkCar(car)
+        );
+        assertEquals("No available position in all parking lots.", exception.getMessage());
+    }
 }
